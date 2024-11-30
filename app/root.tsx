dfbe6699be9b378dbee327831b7502eb90efb630
rafia9005 <rafia9005@gmail.com>
2024-11-30 19:29:23 +0700

    add meta data

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,5 @@
 import {
   Links,
-  Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -13,8 +12,37 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>EduPay - SMKN 6 Malang - Layanan Pembayaran Sekolah Digital</title>
+        <meta
+          name="description"
+          content="EduPay adalah platform pembayaran digital resmi untuk SMKN 6 Malang. Mudahkan proses pembayaran SPP, uang kegiatan, dan layanan lainnya dengan transaksi yang aman, cepat, dan efisien."
+        />
+        <meta property="og:url" content="https://edu-pay.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="EduPay - SMKN 6 Malang - Layanan Pembayaran Sekolah Digital"
+        />
+        <meta
+          property="og:description"
+          content="EduPay adalah platform pembayaran digital resmi untuk SMKN 6 Malang. Mudahkan proses pembayaran SPP, uang kegiatan, dan layanan lainnya dengan transaksi yang aman, cepat, dan efisien."
+        />
+        <meta property="og:image" content="https://edu-pay.vercel.app/logo.jpg" />
+        <meta property="og:image:width" content="1280" />
+        <meta property="og:image:height" content="1280" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="edu-pay.vercel.app" />
+        <meta property="twitter:url" content="https://edu-pay.vercel.app/" />
+        <meta
+          name="twitter:title"
+          content="EduPay - SMKN 6 Malang - Layanan Pembayaran Sekolah Digital"
+        />
+        <meta
+          name="twitter:description"
+          content="EduPay adalah platform pembayaran digital resmi untuk SMKN 6 Malang. Mudahkan proses pembayaran SPP, uang kegiatan, dan layanan lainnya dengan transaksi yang aman, cepat, dan efisien."
+        />
+        <meta name="twitter:image" content="https://edu-pay.vercel.app/logo.jpg" />
         <link rel="icon" href="/logo.jpg" type="image/x-icon" />
-        <Meta />
         <Links />
       </head>
       <body>
@@ -27,7 +55,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return (
-    <Outlet />
-  );
+  return <Outlet />;
 }
+
