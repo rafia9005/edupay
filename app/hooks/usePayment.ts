fd903dc28ab478448cad60c33c2da600d815a867
rafia9005 <rafia9005@gmail.com>
2024-11-30 19:46:02 +0700

    fix snap

--- app/hooks/usePayment.ts
+++ app/hooks/usePayment.ts
@@ -146,7 +146,7 @@ const handleSubmit = async (nisn: string) => {
       checkMonth(nisn, monthsToPay);
       const { token } = response.data;
       if (token) {
-        //window.location.href = `https://app.sandbox.midtrans.com/snap/v2/vtweb/${token}`;
+        window.location.href = `https://app.sandbox.midtrans.com/snap/v2/vtweb/${token}`;
       } else {
         console.error("Failed to get token from server");
         setMessage("Failed to initiate payment.");
