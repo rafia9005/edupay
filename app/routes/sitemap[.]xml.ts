6c39c0c82c9b311157f5f99c47d2ead1012f2fd7
rafia9005 <rafia9005@gmail.com>
2024-12-01 20:42:56 +0700

    fix

--- app/routes/sitemap[.]xml.ts
+++
@@ -1,9 +0,0 @@
-import { routes } from "@remix-run/dev/server-build";
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { generateSitemap } from "@nasa-gcn/remix-seo";
-
-export function loader({ request }: LoaderFunctionArgs) {
-  return generateSitemap(request, routes, {
-    siteUrl: "https://edu-pay.vercel.app",
-  });
-}
